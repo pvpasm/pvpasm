be69dc4aad72b7c7beb3a0ec18e40a7062bf83d0
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2019-02-27 17:34:40 +0800

    Change chall extern function to be system dependent

--- server/grader/pregrade.sh
+++ server/grader/pregrade.sh
@@ -2,8 +2,10 @@
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     arch="elf64"
+    chall="chall"
 else
     arch="macho64"
+    chall="_chall"
 fi
 
 dirname=$1_`date +%s`
@@ -13,9 +15,9 @@ cd $dirname
 
 cp ../template/grader.c .
 
-echo "global _chall" > $3.s
+echo global $chall > $3.s
 echo "section .text" >> $3.s
-echo "_chall:" >> $3.s
+echo $chall >> $3.s
 cat ../../challenges/$2/$3.s >> $3.s
 
 nasm -f $arch $3.s -o challenge.o
