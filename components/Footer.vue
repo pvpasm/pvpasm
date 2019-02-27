127d8a3fff0464b1c8584aef5dbfbde5785c4fb5
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2019-02-27 12:29:14 +0800

    Fix footer

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="footer bg-primary text-white p-3 text-center">
+      &copy; 2019 pvpasm
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,34 @@
 <template>
-  <div style="min-height: 100vh">
-    <div style="min-height: 100vh">
-      <Navbar />
-      
-      <nuxt />
-    </div>
-
-    <div class="bg-primary text-white p-3">
-      pvpasm 2019
-    </div>
+  <div class="app">
+    <Navbar />
+    <nuxt />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 }
 </script>
+
+<style>
+.app {
+  min-height: 100vh;
+  position: relative;
+  padding-bottom: 60px; /* height of your footer */
+}
+
+.footer {
+  position: absolute;
+  right: 0;
+  left: 0;
+  bottom: 0;
+}
+</style>
